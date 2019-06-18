572f1af22eef0f11c7aed16eb290dde802c16904
SD-20170915ONGT\Administrator <718319710@qq.com>
2019-06-18 14:55:22 +0800

    依赖注入示例: p命名空间, c命名空间, java.util.Properties, idref, ref, 内部bean, 复合属性

---
+++ spring-core-example/src/main/java/com/xzz/chaper01/section04/test03/Person.java
@@ -0,0 +1,67 @@
+package com.xzz.chaper01.section04.test03;
+
+public class Person {
+
+	private String name;
+
+	private int age;
+
+	private boolean student;
+
+	private Address address;
+	
+	
+
+	public Person() {
+		/*
+		 * ²âÊÔ¸´ºÏÊôĞÔÉèÖÃ
+		 */
+		//this.address = new Address();
+	}
+
+	public Person(String name, int age, boolean student, Address address) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.student = student;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isStudent() {
+		return student;
+	}
+
+	public void setStudent(boolean student) {
+		this.student = student;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", student=" + student + ", address=" + address + "]";
+	}
+
+}
---
+++ spring-core-example/src/main/java/com/xzz/chaper01/section04/test03/Test03.java
@@ -0,0 +1,34 @@
+package com.xzz.chaper01.section04.test03;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test03 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("section04/test03/beans.xml", "section04/test03/beans2.xml");
+		
+		Person p0 = (Person) ctx.getBean("person0");
+		System.out.println(p0);
+		
+		Person p1 = (Person) ctx.getBean("person1");
+		System.out.println(p1);
+		Person p2 = (Person) ctx.getBean("person2");
+		System.out.println(p2);
+		
+		Person p5 = (Person) ctx.getBean("person5");
+		System.out.println(p5);
+		
+		Person p6 = (Person) ctx.getBean("person6");
+		System.out.println(p6);
+		
+		Person p7 = (Person) ctx.getBean("person7");
+		System.out.println(p7);
+		
+		Person p8 = (Person) ctx.getBean("person8");
+		System.out.println(p8);
+		
+		Person p9 = (Person) ctx.getBean("person9");
+		System.out.println(p9);
+	}
+}
