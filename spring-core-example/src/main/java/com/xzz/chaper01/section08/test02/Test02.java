b3074d27bbba9779d39274bd3cbaffe0523b16f2
SD-20170915ONGT\Administrator <718319710@qq.com>
2019-06-19 14:49:17 +0800

    BeanFactoryPostProcessor操作bean的配置元数据, 例: 覆盖bean的属性定义

---
+++ spring-core-example/src/main/java/com/xzz/chaper01/section08/test02/Test02.java
@@ -0,0 +1,18 @@
+package com.xzz.chaper01.section08.test02;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test02 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("chapter01/section08/test02/beans.xml");
+
+		Person p = (Person) ctx.getBean("person");
+		System.out.println(p);
+		
+		Person p1 = (Person) ctx.getBean("person1");
+		System.out.println(p1);
+	}
+}
